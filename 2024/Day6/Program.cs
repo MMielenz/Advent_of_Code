3a3b16117e05995ee5c2b563279600101654fdf2
mmielenz <Michael.Mielenz@edvschule-plattling.de>
2024-12-06 11:10:24 +0100

    Day6 - start

---
+++ 2024/Day6/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Day6;
+
+class Program
+{
+    public static int Part1()
+    {
+        int result = 0;
+        // List<List<char>> fields = File.ReadAllLines("sample.txt").Select(x => x.ToList());
+
+
+        return result;
+    }
+
+
+    public static int Part2()
+    {
+        int result = 0;
+
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Advent of Code Day 6");
+        Console.WriteLine($"Part 1: {Part1()}");
+        Console.WriteLine($"Part 2: {Part2()}");
+    }
+}
