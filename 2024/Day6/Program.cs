f3cb14c2f7adf0e216212f5ff9f970f34954c462
Michael Mielenz <mmielenz7@gmail.com>
2024-12-06 17:08:15 +0100

    Day 6 - Progress

--- 2024/Day6/Program.cs
+++ 2024/Day6/Program.cs
@@ -2,11 +2,41 @@
 
 class Program
 {
+    class Player(int x, int y)
+    {
+        public int X { get; set; } = x;
+        public int Y { get; set; } = y;
+        public Direction Dir { get; set; } = Direction.Up;
+
+        public void Move()
+        {
+            switch(Dir)
+            {
+                // case Direction.Up:  
+            }
+        }
+
+        public enum Direction
+        {
+            Up, Right, Down, Left
+        }
+    }
+
     public static int Part1()
     {
         int result = 0;
-        // List<List<char>> fields = File.ReadAllLines("sample.txt").Select(x => x.ToList());
+        List<List<char>> field = File.ReadAllLines("sample.txt").Select(x => x.ToCharArray().ToList()).ToList();
+        Player p;
+        for (int i = 0; i < field.Count; i++)
+        {
+            for (int j = 0; j < field[i].Count; j++)
+            {
+                if (field[i][j] == '^') p = new(j, i); break;
+            }
+        }
 
+        
+        
 
         return result;
     }
