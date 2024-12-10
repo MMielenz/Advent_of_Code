8226735c68b65df375dce3c22cfba799891baf0a
mmielenz <Michael.Mielenz@edvschule-plattling.de>
2024-12-10 12:56:30 +0100

    Day 7 - init

---
+++ 2024/Day7/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Day7;
+
+class Program
+{
+    public static int Part1()
+    {
+        int result = 0;
+        return result;
+    }
+
+    public static int Part2()
+    {
+        int result = 0;
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Advent of Code Day 7");
+        Console.WriteLine($"Part 1: {Part1()}");
+        Console.WriteLine($"Part 2: {Part2()}");
+    }
+}
