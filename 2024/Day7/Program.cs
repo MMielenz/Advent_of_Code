0a99a5d4e093f0e2a3929b43fa3d0578c46ab38c
mmielenz <Michael.Mielenz@edvschule-plattling.de>
2024-12-10 20:37:06 +0100

    Day 7 - Part 1

--- 2024/Day7/Program.cs
+++ 2024/Day7/Program.cs
@@ -2,53 +2,84 @@
 
 class Program
 {
-    // public class Term(ulong value, )
     public static ulong Part1()
     {
         ulong result = 0;
-        var data = File.ReadAllLines("sample.txt");
+        var data = File.ReadAllLines("input.txt");
         ulong[] values = data.Select(x => ulong.Parse(x.Split(":")[0])).ToArray();
-        var test = data.Select(x => x.Split(": ")[1]);
         List<int>[] numbers = data.Select(x => x.Split(": ")[1].Split(" ").Select(x => int.Parse(x)).ToList()).ToArray();
-
-        string[] operators = { "+", "*" };
+        string[] operators = ["+", "*"];
 
         for (int i = 0; i < values.Length; i++)
         {
-            int possibleCombos = (int)Math.Pow(operators.Length, numbers[i].Count);
-            int dualLength = Convert.ToString(possibleCombos, 2).Length;
-            ulong erg = 0;
+            int possibleCombos = (int)Math.Pow(operators.Length, numbers[i].Count - 1);
             for (int j = 0; j < possibleCombos; j++)
             {
+                ulong erg = (ulong)numbers[i][0];
                 var operatorList = Convert.ToString(j, 2);
-                operatorList = new string('0', dualLength- operatorList.Length) + operatorList;
-                for(int k = 0; k < operatorList.Length; k++)
+                operatorList = new string('0', numbers[i].Count - 1 - operatorList.Length) + operatorList;
+                for (int k = 0; k < operatorList.Length; k++)
                 {
-                    switch(operatorList[k])
+                    switch (operatorList[k])
                     {
                         case '0':
-                            erg += (ulong)numbers[i][k];
+                            erg += (ulong)numbers[i][k + 1];
                             break;
                         case '1':
-                            erg *= (ulong)numbers[i][k];
+                            erg *= (ulong)numbers[i][k + 1];
                             break;
                     }
                 }
                 if (erg == values[i])
                 {
                     result += erg;
+                    break;
                 }
             }
-            
         }
-
 
         return result;
     }
 
-    public static int Part2()
+    public static ulong Part2()
     {
-        int result = 0;
+        ulong result = 0;
+        var data = File.ReadAllLines("sample.txt");
+        ulong[] values = data.Select(x => ulong.Parse(x.Split(":")[0])).ToArray();
+        List<int>[] numbers = data.Select(x => x.Split(": ")[1].Split(" ").Select(x => int.Parse(x)).ToList()).ToArray();
+        string[] operators = ["+", "*", "||"];
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            int possibleCombos = (int)Math.Pow(operators.Length, numbers[i].Count - 1);
+            for (int j = 0; j < possibleCombos; j++)
+            {
+                ulong erg = (ulong)numbers[i][0];
+                var operatorList = Convert.ToString(j, 2);
+                operatorList = new string('0', numbers[i].Count - 1 - operatorList.Length) + operatorList;
+                for (int k = 0; k < operatorList.Length; k++)
+                {
+                    switch (operatorList[k])
+                    {
+                        case '0':
+                            erg += (ulong)numbers[i][k + 1];
+                            break;
+                        case '1':
+                            erg *= (ulong)numbers[i][k + 1];
+                            break;
+                        case '2':
+                            erg = ulong.Parse(erg.ToString() + numbers[i][k + 1].ToString());
+                            break;
+                    }
+                }
+                if (erg == values[i])
+                {
+                    result += erg;
+                    break;
+                }
+            }
+        }
+
         return result;
     }
 
