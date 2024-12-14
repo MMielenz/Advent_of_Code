4e08274d392f791cbcaec792c86e13bc25df1bdd
Michael Mielenz <mmielenz7@gmail.com>
2024-12-14 12:27:03 +0100

    Day 10 - finished

--- 2024/Day10/Program.cs
+++ 2024/Day10/Program.cs
@@ -2,11 +2,17 @@
 
 class Program
 {
-    public record Position(int X, int Y);
+    public record Position(int X, int Y)
+    {
+        public int GetDiffOfPositions(Position other)
+        {
+            return Math.Abs(X - other.X) + Math.Abs(Y - other.Y);
+        }
+    }
     public static int Part1()
     {
         int result = 0;
-        var data = File.ReadAllLines("sample.txt");
+        var data = File.ReadAllLines("input.txt");
         List<Position>[] positions = new List<Position>[10];
         for (int i = 0; i < data.Length; i++)
         {
@@ -18,21 +24,12 @@ class Program
                 positions[height].Add(new(j, i));
             }
         }
-        // Console.Clear();
-
-
-        // List<Position> trailheads = [.. positions[9]];
-        List<Position> trailhead = positions[9].Select(x => x).ToList();
+        List<Position> trailheads = [.. positions[9]];
         for (int i = 0; i < positions[0].Count; i++)
         {
-            var test = GetScore(positions, 1, positions[0][i], 0); 
-            // result += GetScore(positions, 1, positions[0][i], 0);
-            result += test;
-            // Console.Clear();
-            Console.WriteLine("");
-            positions[9] = trailhead.Select(x => x).ToList();
+            result += GetScore(positions, 1, positions[0][i], 0);
+            positions[9] = [.. trailheads];
         }
-
 
         return result;
     }
@@ -42,44 +39,13 @@ class Program
         if (height == 10)
         {
             positions[9].Remove(lastPosition);
-            // Console.Clear();
             return score + 1;
-
         }
-        for (int i = 0; i < positions[height].Count; i++)
+        for (int i = positions[height].Count - 1; i >= 0; i--)
         {
-            Position p = positions[height][i];
-            // if (score == 2 && p.X == 6 && p.Y == 1 && lastPosition.X == 6 && lastPosition.Y == 2)
-            // {
-            //     Console.WriteLine("");
-            // }
-            if (p.Y == 4 && p.X == 5)
+            if(lastPosition.GetDiffOfPositions(positions[height][i]) == 1)
             {
-                Console.WriteLine("");
-            }
-            if (p.X == lastPosition.X + 1 && p.Y == lastPosition.Y)
-            {
-                // Console.SetCursorPosition(p.X, p.Y);
-                // Console.WriteLine(height);
-                score = GetScore(positions, height + 1, p, score);
-            }
-            if (p.X == lastPosition.X - 1 && p.Y == lastPosition.Y)
-            {
-                // Console.SetCursorPosition(p.X, p.Y);
-                // Console.WriteLine(height);
-                score = GetScore(positions, height + 1, p, score);
-            }
-            if (p.X == lastPosition.X && p.Y == lastPosition.Y + 1)
-            {
-                // Console.SetCursorPosition(p.X, p.Y);
-                // Console.WriteLine(height);
-                score = GetScore(positions, height + 1, p, score);
-            }
-            if (p.X == lastPosition.X && p.Y == lastPosition.Y - 1)
-            {
-                // Console.SetCursorPosition(p.X, p.Y);
-                // Console.WriteLine(height);
-                score = GetScore(positions, height + 1, p, score);
+                score = GetScore(positions, height + 1, positions[height][i], score);
             }
         }
         return score;
