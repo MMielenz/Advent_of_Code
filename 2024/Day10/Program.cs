11993468ff878fa79550d4c104034a5152261f4b
Michael Mielenz <mmielenz7@gmail.com>
2024-12-14 12:44:36 +0100

    Day 10 - finished

--- 2024/Day10/Program.cs
+++ 2024/Day10/Program.cs
@@ -34,27 +34,48 @@ class Program
         return result;
     }
 
-    public static int GetScore(List<Position>[] positions, int height, Position lastPosition, int score)
-    {
-        if (height == 10)
-        {
-            positions[9].Remove(lastPosition);
-            return score + 1;
-        }
-        for (int i = positions[height].Count - 1; i >= 0; i--)
-        {
-            if(lastPosition.GetDiffOfPositions(positions[height][i]) == 1)
-            {
-                score = GetScore(positions, height + 1, positions[height][i], score);
-            }
-        }
-        return score;
-    }
 
     public static int Part2()
     {
         int result = 0;
+        var data = File.ReadAllLines("input.txt");
+        List<Position>[] positions = new List<Position>[10];
+        for (int i = 0; i < data.Length; i++)
+        {
+            for (int j = 0; j < data[i].Length; j++)
+            {
+                if (data[i][j] == '.') continue;
+                int height = int.Parse(data[i][j].ToString());
+                if (positions[height] is null) positions[height] = [];
+                positions[height].Add(new(j, i));
+            }
+        }
+        for (int i = 0; i < positions[0].Count; i++)
+        {
+            result += GetScore(positions, 1, positions[0][i], 0, false);
+        }
+
         return result;
+    }
+
+    public static int GetScore(List<Position>[] positions, int nextHeight, Position lastPosition, int score, bool distinctTrailheads = true)
+    {
+        if (nextHeight == 10)
+        {
+            if (distinctTrailheads)
+            {
+                positions[9].Remove(lastPosition);
+            }
+            return score + 1;
+        }
+        for (int i = positions[nextHeight].Count - 1; i >= 0; i--)
+        {
+            if (lastPosition.GetDiffOfPositions(positions[nextHeight][i]) == 1)
+            {
+                score = GetScore(positions, nextHeight + 1, positions[nextHeight][i], score, distinctTrailheads);
+            }
+        }
+        return score;
     }
 
     static void Main(string[] args)
