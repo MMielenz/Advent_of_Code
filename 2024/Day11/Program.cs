52331b7f3f4be198134337bdb7bb5d7d2eb2981e
Michael Mielenz <mmielenz7@gmail.com>
2024-12-17 21:00:15 +0100

    Day 11 - finished

--- 2024/Day11/Program.cs
+++ 2024/Day11/Program.cs
@@ -30,14 +30,74 @@ int Part1()
 }
 
 
-int Part2()
+ulong Part2()
 {
-    int result = 0;
+    ulong result = 0;
+    List<ulong> AllStones = File.ReadAllLines("input.txt")[0].Split(" ").Select(x => ulong.Parse(x)).ToList();
+    Dictionary<string, ulong> alreadyComputed = [];
+
+    foreach (ulong stone in AllStones)
+    {
+        result += CalculateAmount(stone, 75, alreadyComputed);
+    }
+
     return result;
 }
 
+ulong CalculateAmount(ulong stoneValue, int blink, Dictionary<string, ulong> alreadyComputed)
+{
+    string key = $"{stoneValue}:{blink}";
+
+    if (alreadyComputed.ContainsKey(key))
+    {
+        return alreadyComputed[key];
+    }
+
+    if (blink == 1)
+    {
+        var stones = CalculateStones(stoneValue);
+        ulong stonesCount = (ulong)stones.Count;
+        alreadyComputed.Add(key, stonesCount);
+        return stonesCount;
+    }
+
+    ulong result = 0;
+    var stonesNow = CalculateStones(stoneValue);
+    foreach (var stone in stonesNow)
+    {
+        ulong amount = CalculateAmount(stone, blink - 1, alreadyComputed);
+        result += amount;
+        string newKey = $"{stone}:{blink - 1}";
+        if (alreadyComputed.ContainsKey(newKey)) continue;
+        alreadyComputed.Add(newKey, amount);
+    }
+
+    return result;
+}
+
+List<ulong> CalculateStones(ulong stone)
+{
+    if (stone == 0)
+    {
+        return [1];
+    }
+
+    string number = stone.ToString();
+    if (number.Length % 2 == 0)
+    {
+        ulong num1 = ulong.Parse(number[..(number.Length / 2)]);
+        ulong num2 = ulong.Parse(number.Substring(number.Length / 2, number.Length / 2));
+        return [num1, num2];
+    }
+
+    return [stone * 2024];
+}
 
 Console.WriteLine("Advent of Code Day 11");
+DateTime start = DateTime.Now;
 Console.WriteLine($"Part 1: {Part1()}");
+Console.WriteLine($"Execution Time: {DateTime.Now - start}");
+start = DateTime.Now;
 Console.WriteLine($"Part 2: {Part2()}");
+Console.WriteLine($"Execution Time: {DateTime.Now - start}");
 Console.ReadKey();
