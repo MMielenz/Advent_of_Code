7626df92ff39e67713544f14f67c3eb34ffc9218
Michael Mielenz <mmielenz7@gmail.com>
2024-12-17 22:46:03 +0100

    Added createNewDay.cmd and the template for C# for it

---
+++ templates/csharp_template/Program.cs
@@ -0,0 +1,21 @@
+int Part1()
+{
+    int result = 0;
+
+    return result;
+}
+
+int Part2()
+{
+    int result = 0;
+    
+    return result;
+}
+
+
+DateTime start = DateTime.Now;
+Console.WriteLine($"Part 1: {Part1()}");
+Console.WriteLine($"Execution Time: {DateTime.Now - start}");
+start = DateTime.Now;
+Console.WriteLine($"Part 2: {Part2()}");
+Console.WriteLine($"Execution Time: {DateTime.Now - start}");
